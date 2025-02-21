3b17aeded69a6f33f23023b1df81cb5f3167c87e
Woszuk <hejnaradrian@gmail.com>
2025-02-21 13:41:29 +0100

    Change styling card when task is out of date

---
+++ src/components/atoms/Warning.tsx
@@ -0,0 +1,16 @@
+import WarningAmberIcon from "@mui/icons-material/WarningAmber";
+
+const Warning = () => {
+  return (
+    <WarningAmberIcon
+      sx={{
+        position: "absolute",
+        color: "#ff6666",
+        top: 4,
+        right: 4,
+      }}
+    />
+  );
+};
+
+export default Warning;
--- src/components/molecules/TaskCard.tsx
+++ src/components/molecules/TaskCard.tsx
@@ -2,28 +2,36 @@ import { Card, CardActions, CardContent, Typography } from "@mui/material";
 import { Link } from "react-router";
 import { STATUS, Task } from "src/types/task";
 import { formatDate, isFutureDate } from "src/utils/format-date";
+import Warning from "src/components/atoms/Warning";
 
 type TaskCardProps = {
   task: Task;
 };
 
 const TaskCard = ({ task }: TaskCardProps) => {
+  const shouldDisplayWarning =
+    !isFutureDate(task.dueDate) &&
+    [STATUS.PENDING, STATUS.IN_PROGRESS].includes(task.status) &&
+    task.dueDate;
+
   return (
     <Link to={`/${task.id}`} style={{ textDecoration: "none" }}>
       <Card
         sx={{
           cursor: "pointer",
+          position: "relative",
           marginBottom: 2,
           width: "max-content",
           borderRadius: "10px",
           ":hover": {
-            bgcolor: getHoverColor(task),
+            bgcolor: "#e6e6e6",
           },
           textAlign: "center",
-          bgcolor: getTaskBgColor(task),
+          border: shouldDisplayWarning ? "1px solid #ff6666" : "none",
         }}
       >
         <CardContent>
+          {shouldDisplayWarning && <Warning />}
           <Typography variant="h5">{task.title}</Typography>
           {task.dueDate && (
             <Typography sx={{ fontSize: "13px" }}>
@@ -42,25 +50,3 @@ const TaskCard = ({ task }: TaskCardProps) => {
 };
 
 export default TaskCard;
-
-const getTaskBgColor = (task: Task) => {
-  if (
-    !isFutureDate(task.dueDate) &&
-    [STATUS.PENDING, STATUS.IN_PROGRESS].includes(task.status) &&
-    task.dueDate
-  ) {
-    return "#ff9999";
-  }
-  return "inherit";
-};
-
-const getHoverColor = (task: Task) => {
-  if (
-    !isFutureDate(task.dueDate) &&
-    [STATUS.PENDING, STATUS.IN_PROGRESS].includes(task.status) &&
-    task.dueDate
-  ) {
-    return "#ff6666";
-  }
-  return "#e6e6e6";
-};
