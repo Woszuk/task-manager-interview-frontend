94f2e8e5d38f3c7c15fda785c07f23e50e45316f
Woszuk <hejnaradrian@gmail.com>
2025-02-21 11:50:12 +0100

    Add fetching single task

--- src/api/queries.ts
+++ src/api/queries.ts
@@ -5,6 +5,18 @@ export const GET_TASKS = gql`
     tasks {
       id
       title
+      dueDate
+      status
+      createdAt
+    }
+  }
+`;
+
+export const GET_TASK = gql`
+  query Task($id: String!) {
+    task(id: $id) {
+      id
+      title
       description
       dueDate
       status
--- src/hooks/useTask.ts
+++ src/hooks/useTask.ts
@@ -1,11 +1,21 @@
 import { useQuery } from "@tanstack/react-query";
 import client from "src/api/graphql-client";
-import { GET_TASKS } from "src/api/queries";
+import { GET_TASK, GET_TASKS } from "src/api/queries";
 import { Task } from "src/types/task";
 
 export const useTasks = () => {
   return useQuery<{ tasks: Task[] }>({
     queryKey: ["tasks"],
     queryFn: async () => client.request(GET_TASKS),
+    retry: false,
+  });
+};
+
+export const useTask = (id?: string) => {
+  return useQuery<{ task: Task }>({
+    queryKey: ["task"],
+    queryFn: async () => client.request(GET_TASK, { id }),
+    enabled: !!id,
+    retry: false,
   });
 };
