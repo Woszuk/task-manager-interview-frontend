45e2ed74b70843ea3a8fb775ac72ac7f3ea19e89
Woszuk <hejnaradrian@gmail.com>
2025-02-21 11:49:21 +0100

    Add task timestamp molecule and helper function

---
+++ src/components/molecules/TaskTimestamp.tsx
@@ -0,0 +1,19 @@
+import { Box } from "@mui/material";
+import Timestamp from "src/components/atoms/Timestamp";
+import { isAfter } from "src/utils/format-date";
+
+interface TaskTimestampsProps {
+  createdAt: string;
+  updatedAt: string;
+}
+
+const TaskTimestamps = ({ createdAt, updatedAt }: TaskTimestampsProps) => (
+  <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+    <Timestamp label="Created at" date={createdAt} />
+    {isAfter(updatedAt, createdAt) && (
+      <Timestamp label="Updated at" date={updatedAt} />
+    )}
+  </Box>
+);
+
+export default TaskTimestamps;
--- src/utils/format-date.ts
+++ src/utils/format-date.ts
@@ -3,3 +3,11 @@ import dayjs from "dayjs";
 export const formatDate = (date: string) => {
   return dayjs(date).format("DD/MM/YYYY hh:mm:ss A");
 };
+
+export const isFutureDate = (date: string) => {
+  return dayjs().isBefore(date);
+};
+
+export const isAfter = (date: string, dateToCompare: string) => {
+  return dayjs(date).isAfter(dateToCompare);
+};
