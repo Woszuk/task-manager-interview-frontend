ed519ee0dd3f0b92f4b3c7380a6d55587c45103d
Woszuk <hejnaradrian@gmail.com>
2025-02-21 11:51:43 +0100

    Change main file to handle route

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,32 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import { BrowserRouter, Route, Routes } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Tasks from "src/components/pages/Tasks";
+import Task from "src/components/pages/Task";
+import { Container, Typography } from "@mui/material";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <Container
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          maxWidth: "1440px",
+        }}
+      >
+        <Typography variant="h4" gutterBottom>
+          Task Manager
+        </Typography>
+
+        <Routes>
+          <Route path="/" element={<Tasks />} />
+          <Route path="/:id" element={<Task />} />
+        </Routes>
+      </Container>
+    </QueryClientProvider>
+  </BrowserRouter>
 );
